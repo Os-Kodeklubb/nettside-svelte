0fb9e788fb15e2a3de6ca1d57c57ef71de3c7935
Adrian Sælen <adrianhs@gmail.com>
2022-01-10 14:52:50 +0100

    added about us section

---
+++ src/components/AboutUsSection.svelte
@@ -0,0 +1,110 @@
+<section class="explanation" id="om-oss">
+  <div class="row">
+    <div class="box">
+      <h2>Om Os Kodeklubb</h2>
+      <p>Her kommer litt tekst om Os Kodeklubb</p>
+    </div>
+  </div>
+</section>
+
+<style lang="css">
+
+  .box {
+      background: #e2e2e2;
+      padding: 50px;
+  }
+
+  .box h2 {
+      color: #000;
+      font-family: Arial, Recoleta, sans-serif;
+      padding: 0;
+      margin: 0 0 1.1625rem;
+      text-align: center;
+  }
+
+  .box p {
+      font-size: 1.25em;
+      color: #3c4856;
+      margin: 2.325rem 0 1.1625rem 0;
+      line-height: 1.8;
+      text-align: center;
+      font-family: Arial, Recoleta, sans-serif;
+  }
+
+  .item-list {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      margin: 0;
+      background: #e2e2e2;
+      padding: 50px;
+  }
+
+  .item-list h4 {
+      color: #000;
+      font-family: Arial, Recoleta, sans-serif;
+      margin: 0 0 1rem 0;
+      font-size: 1.25em;
+      font-weight: 600;
+      text-transform: uppercase;
+  }
+  .item-list p {
+      color: #3c4856c2;
+      font-size: 1em;
+      line-height: 1.5;
+      text-align: left;
+      margin: 0.5rem 0 2.3625rem 0;
+  }
+
+  .item {
+      width: 500px;
+  }
+
+  @media only screen and (max-width: 767px) {
+      .item {
+          width: 500px;
+      }
+  }
+
+  @media only screen and (min-width: 768px) {
+      .item {
+          width: 250px;
+      }
+  }
+
+  @media only screen and (min-width: 924px) {
+      .item {
+          width: 300px;
+      }
+  }
+
+  @media only screen and (min-width: 1087px) {
+      .item {
+          width: 350px;
+      }
+  }
+
+  @media only screen and (min-width: 1200px) {
+      .item {
+          width: 400px;
+      }
+  }
+
+  @media only screen and (min-width: 1400px) {
+      .item {
+          width: 450px;
+      }
+  }
+  @media only screen and (min-width: 1401px) {
+      .item {
+          width: 500px;
+      }
+  }
+
+  @media only screen and (min-width: 1740px) {
+      .item {
+          width: 600px;
+      }
+  }
+  
+</style>
