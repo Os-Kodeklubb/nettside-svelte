81db6b432326e4a496df599bd46396b5b737a1ea
Ewy <ewyboy@gmail.com>
2022-01-19 22:56:52 +0100

    Personvern, analytics og smooth scrolling

---
+++ src/routes/personvern.svelte
@@ -0,0 +1,71 @@
+<svelte:head>
+    <title>Personvern</title>
+</svelte:head>
+
+<style lang="scss">
+    h1 {
+        font-size: 3rem;
+        text-align: center;
+    }
+    h2 {
+      font-size: 2rem;
+      text-align: center;
+    }
+    footer {
+      text-align: center;
+    }
+</style>
+
+<div id="header" class="header"></div>
+    <main>
+
+        <section>
+            <header>
+                <h1 class="headliner"><strong>Os Kodeklubb & Dine Data</strong></h1>
+
+                <p class="subtitle" style="margin-left: 15%; margin-right: 15%">
+                    <strong>Ditt personvern er viktig for oss</strong>, og derfor har vi laget denne siden, hvor du kan lese mer om hvordan informasjonen om deg håndteres av Os Kodeklubb.
+                </p>
+            </header>
+        </section>
+
+        <br>
+
+        <article>
+            <h1 class="subliner"><strong>Brukeropplevelse</strong></h1>
+            <p class="subtitle" style="margin-left: 15%; margin-right: 15%">
+                Os Kodeklubb benytter data om hvordan du bruker våre tjenester for å gi deg en bedre brukeropplevelse.
+                Vi jobber kontinuerlig med å forbedre Os Kodeklubb. Gjennom å forstå hvilke tjenester du bruker og hvordan du bruker dem, får vi et bedre bilde av din brukeropplevelse, slik at vi kan tilby deg en best mulig tjeneste og brukeropplevelse.
+            </p>
+        </article>
+
+        <article>
+            <h1 class="subliner"><strong>Analyse av brukerstatistikk</strong></h1>
+            <p class="subtitle" style="margin-left: 15%; margin-right: 15%">
+                Vi jobber kontinuerlig med å vokse, eksponere og markedsføre Os Kodeklubb. Gjennom anonym brukerstatistikk og tjenester som <a class="link" href="https://ads.google.com/home/">Google Ads</a> får vi en bedre forståelse av all datatrafikken til vårt nettsted. Dette hjelper oss med videre vekst og markedsføring.
+            </p>
+        </article>
+
+        <br>
+        <hr>
+        <br>
+
+        <article>
+            <p class="subtitle" style="margin-left: 15%; margin-right: 15%">
+                <strong>Ved å benyttre deg av Os Kodeklubb sitt nettsted godtar du at anonyme data i henhold til The EU General Data Protection Regulation (<a class="link" href="https://gdpr-info.eu/">GDPR</a>) fra 2018 om hvordan du bruker vår nettside blir lagret og brukt til markedsføring, vekst og forbedring av brukeropplevelsen.</strong>
+            </p>
+            <h2 class="subliner" style="margin-left: 15%; margin-right: 15%">
+                Du kan lese mere om dette på lenkene under
+            </h2>
+        </article>
+
+        <footer>
+            <a href="https://gdpr-info.eu/">gdpr-info.eu</a><p></p>
+            <a href="https://eugdpr.org/">eugdpr.org</a><p></p>
+            <a href="https://ec.europa.eu/commission/priorities/justice-and-fundamental-rights/data-protection/2018-reform-eu-data-protection-rules_en">ec.europa.eu</a>
+        </footer>
+
+        <br>
+        <br>
+
+    </main>
