3f92c97ec039c81210d4abfd8f0ef512bb2550b0
dag rune strand <dagrune.strand@gmail.com>
2022-01-20 09:10:21 +0100

    tekst om oss

--- src/components/AboutUsSection.svelte
+++ src/components/AboutUsSection.svelte
@@ -1,8 +1,12 @@
 <section class="explanation" id="om-oss">
   <div class="row">
-    <div class="box">
-      <h2>Om Os Kodeklubb</h2>
-      <p>Her kommer litt tekst om Os Kodeklubb</p>
+    <div class="box item-list">
+      <h2 class="item">Hva er Os Kodekubb?</h2>
+      <div style="border-left:1px solid #000;height:400px"></div>
+      <p class="item">Os Kodeklubb er en klubb for de over 18 år med interesse for
+        koding, og som søker et sosialt kodefellesskap. Klubben er åpen både for
+        nybegynnere og erfarne kodere. På kodeonsdagene fokuserer vi på å lære
+        koding eller lære mer koding. </p>
     </div>
   </div>
 </section>
@@ -10,50 +14,29 @@
 <style lang="css">
 
   .box {
-      background: #e2e2e2;
-      padding: 50px;
+      padding: 70px;
   }
-
-  .box h2 {
-      color: #000;
-      font-family: Arial, Recoleta, sans-serif;
-      padding: 0;
-      margin: 0 0 1.1625rem;
-      text-align: center;
-  }
-
-  .box p {
-      font-size: 1.25em;
-      color: #3c4856;
-      margin: 2.325rem 0 1.1625rem 0;
-      line-height: 1.8;
-      text-align: center;
-      font-family: Arial, Recoleta, sans-serif;
-  }
-
-  .item-list {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-around;
-      margin: 0;
-      background: #e2e2e2;
-      padding: 50px;
-  }
-
   .item-list h4 {
       color: #000;
       font-family: Arial, Recoleta, sans-serif;
       margin: 0 0 1rem 0;
       font-size: 1.25em;
-      font-weight: 600;
       text-transform: uppercase;
   }
   .item-list p {
       color: #3c4856c2;
-      font-size: 1em;
+      font-size: 1.5rem;
       line-height: 1.5;
       text-align: left;
       margin: 0.5rem 0 2.3625rem 0;
+  }
+
+  .item-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 0;
+
   }
 
   .item {
