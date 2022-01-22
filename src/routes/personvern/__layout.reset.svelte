260325d05f169d32ffb754a2985301a487718a38
Torstein <coldasice_t@hotmail.com>
2022-01-22 20:03:56 +0100

    Fix: removed footer from personvern

---
+++ src/routes/personvern/__layout.reset.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from "$lib/header/Header.svelte";
+	import Footer from "$lib/footer/Footer.svelte";
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss" global>
+	@import "../../../static/scss/style.scss";
+
+	main {}
+
+</style>
