e82b5ed77d9f6f6e5b3fc0bc1282b6793e509b11
Adrian Sælen <adrianhs@gmail.com>
2022-03-08 10:59:18 +0100

    trying second form

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -14,7 +14,7 @@
 	}
 </script>
 
-<form id="kontaktskjema" on:submit{handleSubmit} name="Contact - Os Kodeklubb" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+<form id="kontaktskjema" on:submit={handleSubmit} name="Contact - Os Kodeklubb" method="POST" data-netlify="true" netlify-honeypot="bot-field">
   <div class="container">
       <input type="text" name="name" placeholder="Navn" />
       <input type="email" name="email" placeholder="Email" />
@@ -23,4 +23,21 @@
   <textarea name="message" placeholder="Melding" />
   <button type="submit">Send Melding</button>
   <input type="hidden" name="form-name" value="Contact - Os Kodeklubb">
+</form>
+
+<form name="test" method="post" netlify netlify-honeypot="bot-field">
+  <input type="hidden" name="form-name" value="test" />
+  <input type="text" name="bot-field" />
+  <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+      <label>Message: <textarea name="message" /></label>
+  </p>
+  <p>
+      <button type="submit">Send</button>
+  </p>
 </form>
