f449ba2b6104ce75cfe167824f01ce70844e0599
Adrian Sælen <adrianhs@gmail.com>
2022-03-08 10:42:20 +0100

    changed name on form

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -12,7 +12,7 @@
 	}
 </script>
 
-<form id="kontaktskjema" on:submit={handleSubmit} name="Kontakt - Os Kodeklubb" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+<form id="kontaktskjema" on:submit{handleSubmit} name="Kontakt - Os Kodeklubb" method="POST" data-netlify="true" netlify-honeypot="bot-field">
   <div class="container">
     <input type="text" name="name" placeholder="Navn" />
     <input type="email" name="email" placeholder="Email" />
@@ -20,5 +20,5 @@
   </div>
   <textarea name="message" placeholder="Melding" />
   <button class="mx-2 bg-red-50 border-2 px-2 rounded-md  block mx-auto" type="submit">Send Melding</button>
-  <input type="hidden" name="form-name" value="Contact - Green Guys">
+  <input type="hidden" name="form-name" value="Kontakt - Os Kodeklubb">
 </form>
